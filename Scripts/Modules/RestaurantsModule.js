134bd3e64ce342ec91a9e1f6972c22b70b237441
TerjeSlaatto <Terjeslaatto@gmail.com>
2021-05-19 12:13:45 +0200

    RestaurantsModule done

---
+++ Scripts/Modules/RestaurantsModule.js
@@ -0,0 +1,43 @@
+const RestaurantModule = ( function() {
+
+    const restaurantsInfoArray = [
+        {
+            name: "Gyldne Pizza Karl Johan",
+            manager: ,
+            adress: "Karl Johansgate 42",
+            phoneNr: "+4724242424",
+            eMail: "karljohan@gp.no",
+            openingHours: "Mon-fri: 14:00 - 22:00. Sat-Sun: 12:00 - 21:00",
+            nmbrOfEmployees: 3
+        },
+        {
+            name: "Gyldne Pizza Bjørvika",
+            manager: ,
+            adress: "Bjørvikagata 34, 0444 Oslo",
+            phoneNr: "+4723232323",
+            eMail: "bjorvika@gp.no",
+            openingHours: "Mon-fri: 14:00 - 22:00. Sat-Sun: 12:00 - 21:00" ,
+            nmbrOfEmployees: 3
+        },
+        {
+            name: "Gyldne Pizza Nydalen",
+            manager: ,
+            adress: "Nydalenveien 59",
+            phoneNr: "+4712131415",
+            eMail: "nydalen@gp.no",
+            openingHours: "Mon-fri: 14:00 - 22:00. Sat-Sun: 12:00 - 21:00",
+            nmbrOfEmployees: 3
+        },
+        {
+            name: "Gyldne Pizza Grunerløkka",
+            manager: ,
+            adress: "Ryes Plass 42",
+            phoneNr: "+4755554444" ,
+            eMail: "grunerlokka@gp.no",
+            openingHours: "Mon-fri: 14:00 - 22:00. Sat-Sun: 12:00 - 21:00",
+            nmbrOfEmployees: 3
+        }
+
+    ]
+
+}())
