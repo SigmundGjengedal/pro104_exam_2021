ac72dfaa35b8ffe6af5292a9cfafd26e1397237a
Velpre <veljko_kv@hotmail.com>
2021-05-20 11:11:29 +0200

    commit etter sammenslåing med Terje

--- index.js
+++ index.js
@@ -1,6 +1,49 @@
-var loginInput = document.getElementById("#test");
-console.log(loginInput);
 
-loginInput.addEventListener("click", ()=>{
-  alert();
-});
+const loginBtn = document.querySelector(".login_btn")
+const signinUnderline = document.querySelector(".signin_underline");
+const signInBtn = document.querySelector(".signin_btn");
+const createBtn = document.querySelector(".create_btn");
+
+
+const passwordInput = document.querySelector(".login_password");
+const usernameInput = document.querySelector(".login_input");
+const form = document.querySelector("#form")
+
+const passwordEye = document.querySelector("#toggle_password_eye");
+
+
+
+//funksjon for sign in / create underline
+//dette kan vel kombineres til kortere kode
+
+createBtn.addEventListener("click", function(){
+    signinUnderline.classList.toggle("signin_underline_create")
+})
+signInBtn.addEventListener("click", function(){
+    signinUnderline.classList.toggle("signin_underline_create")
+})
+
+
+//Button starter som disabled, når minimum-length på passord
+//og brukernavn er tastet inn blir knappen aktivert.
+loginBtn.disabled = true;
+form.addEventListener("keyup", function(){
+    let username = usernameInput.value;
+    let password = passwordInput.value;
+    if(username.length > 2 && password.length > 2){
+        loginBtn.disabled = false;
+        loginBtn.style.opacity = "1";
+    }else {
+        loginBtn.disabled = true;
+        loginBtn.style.opacity = "0.2";
+    }
+})
+
+
+//Toggle vising av passord.
+passwordEye.addEventListener("click", function(e){
+
+    const type = passwordInput.getAttribute("type") === 'password' ? "text" : "password";
+    passwordInput.setAttribute("type", type);
+    passwordEye.classList.toggle("fa-eye-slash");
+})
