47fd55045d1828766a4a862c4429996221b327a3
TerjeSlaatto <Terjeslaatto@gmail.com>
2021-05-21 16:20:57 +0200

    må sjekke noe i main

--- Scripts/messages.js
+++ Scripts/messages.js
@@ -1,5 +1,6 @@
 const message1 = document.querySelector(".message_1");
 
 message1.addEventListener("click", () =>{
+    document.querySelector("body").style.opacity = "0.2"
     message1.classList.toggle("message_1_expanded")
 })
