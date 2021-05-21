c789358e3ae42ac4ad532f946d4941eb25a07a7a
TerjeSlaatto <Terjeslaatto@gmail.com>
2021-05-21 13:55:02 +0200

    counter done

---
+++ Scripts/counter.js
@@ -0,0 +1,32 @@
+import SalesModule from './Modules/SalesModule.js';
+
+const positiveProfit = document.querySelector(".positive_profit");
+const totalProfitNumber = document.querySelector(".total_profit_number");
+
+function totalNumber(){
+const salesArray = SalesModule.getAll();
+const totalProfit = salesArray[0].revenueAccThisYear + salesArray[1].revenueAccThisYear + salesArray[2].revenueAccThisYear + salesArray[3].revenueAccThisYear;
+return totalProfit;
+}
+
+function makeNumbers(){
+    let numbers = [];
+    for(let i=31;i>0;i--){
+        numbers.push(totalNumber() / i)
+    }
+    return numbers;
+}
+
+function loopNumber(){
+    var counter = 0;
+    var numbers = makeNumbers();
+    var loopnumber = setInterval( changeNumbers, 80);
+    function changeNumbers() {
+        counter++;
+        if(counter === 30){
+            clearInterval(loopnumber)
+        }
+        totalProfitNumber.innerHTML = Math.floor(numbers[counter]) + ",-";
+    }
+}
+loopNumber();
