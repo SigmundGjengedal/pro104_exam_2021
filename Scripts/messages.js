ac9c8f2e4526c2a95066a816a273025a2dc441d1
Velpre <veljko_kv@hotmail.com>
2021-05-23 11:29:55 +0200

    jobber med slicing funksjon og posisjonering av meldinger

--- Scripts/messages.js
+++ Scripts/messages.js
@@ -3,11 +3,22 @@
 const overlay = document.querySelector("#overlay");
 const message3 = document.querySelector(".message_3");
 const inboxName = document.querySelector(".inbox_name");
+const messageTxt = document.querySelectorAll(".message_txt");
+const expandable = document.querySelectorAll(".expandable");
 
-document.querySelectorAll(".expandable").forEach(element => {
+// Function som slicer og printer message text til x antall bokstaver - Må jobbes mere med
+function slicingMsgTxt(){
+  for (var i = 0; i < messageTxt.length; i++) {
+    let slicedTxt = messageTxt[i].innerHTML.slice(0,50);
+    messageTxt[i].innerHTML = slicedTxt + "...";
+  }
+};
+
+
+expandable.forEach(element => {
 element.addEventListener("click", function(e){
     this.classList.toggle("expanded");
-    document.querySelector("#overlay").classList.toggle("overlay")
+    overlay.classList.toggle("overlay");
 })
 })
 
@@ -26,21 +37,16 @@ element.addEventListener("click", function(e){
 overlay.addEventListener("click", () =>{
     // message1.classList.toggle("message_1_expanded");
     // message2.classList.toggle("message_1_expanded");
-    document.querySelectorAll(".expandable").forEach(element => element.classList.remove("expanded"));
-    document.querySelector("#overlay").classList.toggle("overlay");
+    expandable.forEach(element => element.classList.remove("expanded"));
+    overlay.classList.toggle("overlay");
+    slicingMsgTxt();
 });
-
-
 
 // Koden som henter username fra localStorage og printer det ut i inbox delen
 window.onload = inboxName.innerHTML = localStorage.getItem("username");
 
-
 // Time function som printer ut melding etter vis antal sekunder
-
 function printMessage(){
 message3.style.visibility="visible";
-
 };
-
 window.setTimeout(printMessage, 3000);
