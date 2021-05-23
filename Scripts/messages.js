6c36ddc001c0854934ea526c2feddfea6b16e28a
Velpre <veljko_kv@hotmail.com>
2021-05-23 17:44:40 +0200

    Dato er ferdig kodet

--- Scripts/messages.js
+++ Scripts/messages.js
@@ -6,19 +6,78 @@ const inboxName = document.querySelector(".inbox_name");
 const messageTxt = document.querySelectorAll(".message_txt");
 const expandable = document.querySelectorAll(".expandable");
 
+
+
 // Function som slicer og printer message text til x antall bokstaver - Må jobbes mere med
+const msgArray = [
+   "Kenneth er syk idag. Kunne du sett om vi har noen som kan erstate kveldsvakta hans?",
+   "Kunne vi flyttet neste onsdags møte til torsdag? Jeg må være på restorangen fram til kl.12, men etter det er jeg ledig når som helst.",
+   "Hei. Prøvde å ringe. Kan du ringe meg så fort du ser dette?Takk"
+];
 function slicingMsgTxt(){
   for (var i = 0; i < messageTxt.length; i++) {
-    let slicedTxt = messageTxt[i].innerHTML.slice(0,50);
+    let slicedTxt = msgArray[i].slice(0,50);
     messageTxt[i].innerHTML = slicedTxt + "...";
   }
 };
+window.onload = slicingMsgTxt();
+
+// Angir dato for meldinger i inboksen
+const months = [
+  'Jan',
+  'Feb',
+  'Mar',
+  'Apr',
+  'May',
+  'Jun',
+  'Jul',
+  'Augt',
+  'Sep',
+  'Oct',
+  'Nov',
+  'Dec'
+];
+
+let today = new Date();
+const dd = String(today.getDate()).padStart(2, '0');
+const mIndex = today.getMonth();
+const mm = months[mIndex];
+
+const hours = String(today.getHours());
+const minutes = String(today.getMinutes());
+
+today = dd + " " + mm;
+const timeToday = hours + ":" + minutes;
+
+console.log(today);
+console.log(timeToday);
+
+// simulerer gårsdagens dato som brukes på 2 av meldingene
+let yesterday = new Date();
+yesterday.setDate(yesterday.getDate() - 1);
+
+const ddYesterday = String(yesterday.getDate()).padStart(2, '0');
+const mmIndexYesterday = yesterday.getMonth();
+const mmYesterday = months[mmIndexYesterday];
+
+yesterday = ddYesterday + " " + mmYesterday;
+console.log(yesterday);
 
 
+
+
+
+
+
+
+
+// Toggler expanded class som åpner og stenger inbox meldinger
 expandable.forEach(element => {
 element.addEventListener("click", function(e){
+    console.log(e.currentTarget);
     this.classList.toggle("expanded");
     overlay.classList.toggle("overlay");
+
 })
 })
 
