f187a46952e604e195f4bed7e6d8a3315fc04144
SigmundGjengedal <sigmund.gjengedal@gmail.com>
2021-05-28 15:37:55 +0200

    fikset feilein

--- Scripts/sales-page.js
+++ Scripts/sales-page.js
@@ -169,7 +169,7 @@ calculate_Btn.addEventListener('click', (event)=> {
 
     restaurant = "";
     const fillDiagram = function(){
-        document.querySelector(".diagram_1_filled").style.width = result/10000+'%';
+        document.querySelector('.sales_diagram_1_filled').style.width = result/10000+'%';
     }
     
     setTimeout(fillDiagram,100);
@@ -186,90 +186,12 @@ calculate_Btn.addEventListener('click', (event)=> {
 
 })
 
-/*Genererer start-innhold på siden*/
-
-
-
-
-
-/********************* Funksjonalitet på siden *********************************/
+/********************* variabler på siden *********************************/
 
     let restaurant = "";
     let period = "";
     let category = "";
-    /* Henter verdiene fra knappene via bubbling */
 
-
-    restaurant_choice.addEventListener('click', (event) => {
-        const isButton = event.target.nodeName === 'BUTTON';
-        if (!isButton) {
-          return;
-        }
-        restaurant = event.target.id;
-        chosenDiv.innerHTML +=`
-        <button class="buttonChosen">${restaurant}</button>
-        `;
-
-        restaurant_choice.style.opacity = 0.5;
-        console.log(restaurant);
-    })
-
-
-
-    periode_choice.addEventListener('click', (event) => {
-        const isButton = event.target.nodeName === 'BUTTON';
-        if (!isButton) {
-          return;
-        }
-        period = event.target.id;
-        chosenDiv.innerHTML +=`
-        <button class="buttonChosen">${period}</button>
-        `;
-        periode_choice.style.opacity = 0.5;
-        console.log(period);
-    })
-            
-    category_choice.addEventListener('click', (event) => {
-        const isButton = event.target.nodeName === 'BUTTON';
-        if (!isButton) {
-          return;
-        }
-        category = event.target.id;
-
-        chosenDiv.innerHTML +=`
-        <button class="buttonChosen">${category}</button>
-        `;
-        category_choice.style.opacity = 0.5;
-        console.log(category);
-    })
-
-
-    // bruker verdiene hentet fra knappene når man trykker calculate!
-    calculate_Btn.addEventListener('click', (event)=> {
-
-        /* kaller funksjonen med verdier fra knapper.*/
-        let result = SalesModule.getRevenueByRestaurantAndPeriodAndCategory(restaurant,period,category);
-    
-        /*printer*/
-        resultText.innerHTML = `
-        <p> <span class = "result-text-span">${period}'s total </span> for <span class = "result-text-span">${category}</span>  at <span class = "result-text-span">${restaurant}</span>  was <span class="result_number"> ${result}</span> NOK</p>
-        `;
-        /* fyller diagram*/
-
-        restaurant = "";
-        const fillDiagram = function(){
-            document.querySelector(".diagram_1_filled").style.width = result/10000+'%';
-        }
-        
-        setTimeout(fillDiagram,100);
-
-        /* resetter styling på siden*/
-
-        chosenDiv.innerHTML = "";
-        category_choice.style.opacity = 1;
-        restaurant_choice.style.opacity = 1;
-        periode_choice.style.opacity = 1;
-    })
 
 
 
