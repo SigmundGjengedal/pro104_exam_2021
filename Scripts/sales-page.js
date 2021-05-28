b27ee83a15c690b84194ccec5b3bd8a86064f1e6
SigmundGjengedal <sigmund.gjengedal@gmail.com>
2021-05-28 12:37:58 +0200

    v1 av integrert side

--- Scripts/sales-page.js
+++ Scripts/sales-page.js
@@ -1,16 +1,112 @@
 import SalesModule from './Modules/SalesModule.js';
 
+/*Henter main content div fra home */
+const mainContent = document.querySelector(".main_content");
+
+/* henter diverse elementer*/
 const resultText = document.querySelector(".result-text");
 const chosenDiv = document.querySelector("#selected");
 
 /* henter buttons*/
-
 const restaurant_choice = document.querySelector(".restaurant-choice");
 const periode_choice = document.querySelector(".period-choice");
 const category_choice = document.querySelector(".category-choice");
-
 const calculate_Btn = document.querySelector(".calculate-btn");
 
+/*Genererer start-innhold på siden*/
+
+let mainTextHome = `
+
+<div class="salesPage_Container">
+
+<header>
+    <h3 id="Sales_Heading">SALES</h3>  
+    <p id="Heading-p">Please select Restaurant, Period and Category</p>
+</header>
+
+<!-- navn på kolonner-->
+<div class="labels-choices">
+    <span class ="choice restaurant">Restaurant</span>
+    <span class ="choice period">Period </span>
+    <span class ="choice category">Category</span>
+ </div>
+
+<!-- div som skal fylles med valg-->
+
+<section class="flex-columns">
+    <div class="row">
+
+        <div class="column">
+            <div class="column-1">
+                <h4>Chosen</h4>
+                <div id = "selected" class="data_container">
+                       
+                </div>
+                <button class="button calculate-btn">Calculate</button>
+            </div>
+        </div>
+
+         <!-- brukervalg, til høyre på siden-->
+        
+            <div class="select-columns restaurant-choice">
+                <div class="data_container">
+                    <button id="Bjørvika" class="button restaurant-button">BJØRVIKA</button>
+                    <button id="Nydalen" class="button restaurant-button">NYDALEN</button>
+                    <button id="Grunerløkka" class="button restaurant-button">GRUNERLØKKA</button>
+                    <button id="Karl-Johan" class="button restaurant-button">KARL JOHAN</button>
+                </div>
+            </div>
+        
+            <div class="select-columns period-choice">
+                <div class="data_container">
+                    <button id="this-year" class="button">THIS YEAR</button>
+                    <button id="last-year" class="button">LAST YEAR</button>
+                    <button id="last-month" class="button">LAST MONTH</button>
+                    <button id="last-week" class="button">LAST WEEK</button>
+                </div>
+            </div>
+
+            <div class="select-columns category-choice">
+                <div class="data_container">
+                    <button id="all" class="button">ALL</button>
+                    <button id="pizza" class="button">PIZZA</button>
+                    <button id="drinks" class="button">DRINKS</button>
+                    <button id="salads" class="button">SALADS</button>
+                </div>
+            </div>
+        
+    </div>
+</section>
+    
+
+        <!-- % filled til søylene  må genereres-->
+        <section class="result-section">
+
+                <div class="diagram_1">
+                    <div class="diagram_1_filled"></div>
+                 </div>
+
+                   <!-- tallet må tilpasses hva som vises, mnd, år, osv -->
+                   <div class="sales_numbers_container">
+                     <span class="sales_number">0</span>
+                     <span class="sales_number">250K</span>
+                     <span class="sales_number">500K</span>
+                     <span class="sales_number">750K</span>
+                     <span class="sales_number">1000k</span>
+                  </div>
+
+                  <!-- tall/ text må må tilpasses hva som vises, mnd, år, osv -->
+                  <div class="result-text">
+                    
+                  </div>         
+        </section>
+</div>
+
+`;
+
+mainContent.innerHTML = mainTextHome;
+
+/********************* Funksjonalitet på siden *********************************/
 
     let restaurant = "";
     let period = "";
@@ -87,8 +183,6 @@ const calculate_Btn = document.querySelector(".calculate-btn");
     })
 
 
-
-/*****************************************  prøving og feiling under her:   enkel test med kun med ett parameter *****************************************/
 
 
 
